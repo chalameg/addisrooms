382ac10b6b85050a5d0afe79a62700ef65e43299
Chalameg <chalamegersa5@gmail.com>
2022-04-28 20:58:44 +0300

    room card design

--- client/src/components/Room.js
+++ client/src/components/Room.js
@@ -3,7 +3,29 @@ import React from 'react'
 function Room({room}) {
   return (
     <div>
-        <h1>{room.name}</h1>
+        <div className="row">
+          <div className="col-md-4">
+            <img src={room.images[0]} alt="" />
+          </div>
+          <div className="col-md-8">
+            <h1>{room.name}</h1>
+            <span>Max Count: {room.maxcount}</span>
+            <p>Phone Number: {room.phonenumber}</p>
+            <p>TYpe: {room.type}</p>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-8">
+            
+          </div>
+          <div className="col-md-2">
+          <button>book now</button>
+          </div>
+          <div className="col-md-2">
+            <button>view details</button>
+            
+          </div>
+        </div>
     </div>
   )
 }
